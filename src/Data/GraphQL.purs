b531b8bd20ca3408db53aa0b29bd3d5c3815f49d
hendrikniemann <h.neverman@web.de>
2018-08-06 01:56:11 +0200

    Create some documentation
    The docs willbe here until we can publish

--- src/Data/GraphQL.purs
+++ src/Data/GraphQL.purs
@@ -46,18 +46,29 @@ foreign import intScalar :: GraphQLType Int
 foreign import floatScalar :: GraphQLType Number
 foreign import idScalar :: GraphQLType String
 
+-- | Parse a query string and return a parsed GraphQL document or an error.
 parse :: String -> Either Error Document
 parse = runFn3 nativeParse Left Right
 
+-- | Validate the AST of a query against a given schema. Returns an array of
+-- | errors. The array is empty if all validation pass. Currently only supports
+-- | the default validations from `graphql-js`.
 validate :: Schema -> Document -> Array Error
 validate = runFn2 nativeValidate
 
+-- | Asyncroniously executes a query given the GraphQL document and a schema.
 execute :: Schema -> Document -> Aff Json
 execute scm document = fromEffectFnAff $ runFn2 nativeExecute scm document
 
+-- | Create a schema given a root query object type and a root mutation type.
+-- | Schemas don't need a mutation type therefore it is optional.
 schema :: GraphQLType Unit -> Maybe (GraphQLType Unit) -> Schema
 schema query mutation = runFn2 nativeSchema query $ toNullable mutation
 
+-- | Create a new object type with the following properties:
+-- | - `name` is the name of the object in the schema
+-- | - `descrition` is the description of the type
+-- | - `fields` is a record of field definitions
 objectType :: ∀ a r. Homogeneous r (Field a)
   => String
   -> Maybe String
@@ -66,6 +77,22 @@ objectType :: ∀ a r. Homogeneous r (Field a)
 objectType name description =
     runFn3 nativeObjectType name $ toNullable description
 
+-- | Create a simple field without arguments using
+-- | - `type` the type of the field
+-- | - `description` the description of the field
+-- | - `resolve` a function resolving the value of the field
+-- |
+-- | *Examples*
+-- | ``` purescript
+-- | hello :: Field Unit
+-- | hello = field' stringScalar Nothing \_ -> pure "Hello World"
+-- |
+-- | newtype User = User { name :: String, age :: Int }
+-- | name :: Field User
+-- | name = field' intScalar (Just "Age of the user") resolve
+-- |   where
+-- |     resolve (User user) = pure user.name
+-- | ```
 field' :: ∀ a b . GraphQLType b -> Maybe String -> (a -> Aff b) -> Field a
 field' t description resolve =
     runFn4 nativeField t (toNullable description) {} $
