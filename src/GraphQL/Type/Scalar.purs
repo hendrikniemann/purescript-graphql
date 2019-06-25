2b2ecf3e7530caebcac3e22228c72ead2e4d5a75
hendrikniemann <h.neverman@web.de>
2019-06-25 17:31:54 +0200

    Create float and id scalar
    We now support all four standard scalars defined by the
    GraphQL specification.

--- src/GraphQL/Type/Scalar.purs
+++ src/GraphQL/Type/Scalar.purs
@@ -4,7 +4,8 @@ import Prelude
 
 import Data.Argonaut.Core as Json
 import Data.Either (Either(..), note)
-import Data.Int (fromNumber, fromString, toNumber)
+import Data.Int as Int
+import Data.Number as Number
 import Data.Maybe (Maybe(..))
 import GraphQL.Language.AST as AST
 import GraphQL.Type (ScalarType(..))
@@ -15,7 +16,7 @@ string = ScalarType { name, description, parseLiteral, parseValue, serialize }
   name = "String"
   description = Just "Built in string scalar type."
   parseLiteral (AST.StringValueNode { value }) = pure value
-  parseLiteral _ = Left "Expected input string."
+  parseLiteral _ = Left "Expected string value node for input type String."
   parseValue = Json.caseJsonString (Left "Expected string JSON value.") pure
   serialize = Json.fromString
 
@@ -25,8 +26,30 @@ int = ScalarType { name, description, parseLiteral, parseValue, serialize }
   name = "Int"
   description = Just "Built in integer scalar type."
   parseLiteral (AST.IntValueNode { value }) =
-    note "Expected integer value within the JavaScript Int bounds." $ fromString value
-  parseLiteral _ = Left "Expected integer node."
+    note "Expected integer value within the JavaScript Int bounds." $ Int.fromString value
+  parseLiteral _ = Left "Expected integer value node for input type Int."
   parseValue = Json.caseJsonNumber (Left "Expected number JSON value.")
-    (note "Expected integer value but got a floating point value" <<< fromNumber)
-  serialize = Json.fromNumber <<< toNumber
+    (note "Expected integer value but got a floating point value" <<< Int.fromNumber)
+  serialize = Json.fromNumber <<< Int.toNumber
+
+float :: ScalarType Number
+float = ScalarType { name, description, parseLiteral, parseValue, serialize }
+  where
+  name = "Float"
+  description = Just "Built in float scalar type."
+  parseLiteral (AST.FloatValueNode { value }) =
+    note "Expected float value within the JavaScript number bounds." $ Number.fromString value
+  parseLiteral _ = Left "Expected float value node for input type Float."
+  parseValue = Json.caseJsonNumber (Left "Expected number JSON value.") pure
+  serialize = Json.fromNumber
+
+id :: ScalarType String
+id = ScalarType { name, description, parseLiteral, parseValue, serialize }
+  where
+  name = "ID"
+  description = Just "Built in ID scalar type, accepts both integers and strings."
+  parseLiteral (AST.StringValueNode { value }) = pure value
+  parseLiteral (AST.IntValueNode { value }) = pure value
+  parseLiteral _ = Left "Expected string or integer value node for input type ID."
+  parseValue = Json.caseJsonString (Left "Expected string or integer JSON value.") pure
+  serialize = Json.fromString
